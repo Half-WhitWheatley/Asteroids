6b2857bff41753e38e902ce720b232a458fe3a82
Half-WhitWheatley <whiton@odiorne.net>
2026-07-22 20:46:08 -0400

    added scoring and insults

---
+++ insults.py
@@ -0,0 +1,13 @@
+import random
+
+insults = [
+    "Git good, scrub",
+    "Pathetic",
+    "Dishonor on you! Dishonor on your cow!",
+    "You put the Ram in Dodge.",
+    "You utilize the space bar as well as you use the space between your ears!"
+    
+]
+
+def get_insult():
+    return random.choice(insults)
--- main.py
+++ main.py
@@ -5,9 +5,13 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
+from insults import get_insult
 import sys
+import os
 
 def main():
+    pygame.init()
+    pygame.font.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -25,8 +29,10 @@ def main():
     Shot.containers = (shots, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
+    score: int = 0
     while True:
         log_state()
+        font = pygame.font.Font(None, 36)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -38,13 +44,17 @@ def main():
         for a in asteroids:
             if player.collides_with(a):
                 log_event("player_hit")
-                print("Game over!")
+                print(f"Your score: {score}")
+                print(get_insult())
                 sys.exit()
             for s in shots:
                 if s.collides_with(a):
                     log_event("asteroid_shot")
                     a.split()
                     s.kill()
+                    score += a.radius // 2
+        score_text = font.render(f'Score: {score}', True, (255, 255, 255))
+        screen.blit(score_text, (10, 10))
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
